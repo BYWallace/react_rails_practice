cd83a9c859016195220c1554933564c53fd6847b
Brett Wallace <bwallace90@gmail.com>
2016-11-26 12:00:57 -0500

    Implement adding a new item via POST

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -1,8 +1,24 @@
 var NewItem = React.createClass({
+  handleClick() {
+    var name = this.refs.name.value;
+    var description = this.refs.description.value;
+
+    $.ajax({
+      url: '/api/v1/items',
+      type: 'POST',
+      data: { item: { name: name, description: description} },
+      success: (response) => {
+        console.log('it worked!', response);
+      }
+    });
+  },
+
   render() {
     return (
       <div>
-        <h1>New Item</h1>
+        <input ref='name' placeholder='Enter a name' />
+        <input ref='description' placeholder='Enter a description' />
+        <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
